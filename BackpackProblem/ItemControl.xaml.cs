76df792cf7d27e11c3d4d714aea32c138b2ae6bc
Алексей Лаптев <radio.volchanka@gmail.com>
2019-02-22 16:08:08 +0500

    first commit

---
+++ BackpackProblem/ItemControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackpackProblem
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemControl.xaml
+    /// </summary>
+    public partial class ItemControl : UserControl
+    {
+        public ItemControl()
+        {
+            InitializeComponent();
+        }
+
+        private void bRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            (Parent as ListBox).Items.Remove(this);
+        }
+    }
+}
